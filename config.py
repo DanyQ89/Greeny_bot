259b1fe78e32b7c57eafb0da5e88a3165828d7f6
Danila <108182297+DanyQ89@users.noreply.github.com>
2025-07-14 13:44:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # greeny_official_bot
-token = '7095280804:AAHWpx7g2CkJgApvO_b5V_o36Lj_-azhrxk'
+token = ''
 
 # greomatchbot
 # token = '6994388651:AAFIJpcgnl7vTH1_s-glg1oE6Zi5gwLSTMU'
 
 #YooKassa
-shop_id = '395900'
-secret_key = 'live_8QzBuvpXyepGCSf_nrvrMct386_Cgbt0ntcSVl19RAk'
+shop_id = ''
+secret_key = ''
